228a2c8127c1ef6b8c7401114eaab3a9b7390d08
Adrien SÉON <adrien.seon@gmail.com>
2019-06-15 17:26:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,6 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.all("*", function(req, res, next) {
-  if (req.method.toLowerCase() !== "options") {
-    return next();
-  }
-  return res.send(204);
-});
-
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
