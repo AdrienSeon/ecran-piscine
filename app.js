6f463f06d3845afaedc97623a9a1e9b7915aaab9
Adrien SÉON <adrien.seon@gmail.com>
2019-06-16 17:24:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var getIndex = require('./routes/index');
 var getObixPointDataRouter = require('./routes/get-obix-point-data');
 
 var app = express();
@@ -11,8 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.use('/', getIndex);
 app.use("/get-obix-point-data", getObixPointDataRouter);
 
 module.exports = app;
