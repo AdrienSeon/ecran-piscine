34d7af1e144d4ff3c45643d005c48facf7466685
Adrien SÉON <adrien.seon@gmail.com>
2019-06-24 23:51:01 +0200

    Update CentralEditor.route.js

--- routes/CentralEditor.route.js
+++ routes/CentralEditor.route.js
@@ -6,9 +6,9 @@ let CentralEditor = require('../utils/dbModels/CentralEditor.model');
 centralEditorRoutes.route('/').get(function (req, res) {
 	CentralEditor.find(function(err, editor){
 		if (err) {
-			console.log(err);
+			res.status(400).json(err);
 		} else {
-			res.json(editor);
+			res.status(200).json(editor);
 		}
 	});
 });
@@ -16,19 +16,17 @@ centralEditorRoutes.route('/').get(function (req, res) {
 centralEditorRoutes.route('/get-editor').get(function (req, res) {
 	CentralEditor.find(function(err, editor){
 		if (err) {
-			console.log(err);
+			res.status(400).json(err);
 		} else {
-			res.json(editor[0]);
+			res.status(200).json(editor[0]);
 		}
 	});
 });
 
 centralEditorRoutes.route('/save').post(function (req, res) {
 	CentralEditor.countDocuments(function (err, count) {
-
 		if (!err && count === 0) {
 			let editor = new CentralEditor(req.body);
-
 			editor.save()
 				.then(editor => {
 					res.status(200).json({'editor': 'editor in added successfully'});
@@ -46,12 +44,13 @@ centralEditorRoutes.route('/save').post(function (req, res) {
 							res.status(404).send("data is not found");
 						else {
 							editor.editorContent = req.body.editorContent;
-							editor.save().then(editor => {
-								res.json('Update complete');
-							})
-							.catch(err => {
-								res.status(400).send("Unable to update the database");
-							});
+							editor.save()
+								.then(editor => {
+									res.status(200).json('Update complete');
+								})
+								.catch(err => {
+									res.status(400).send("Unable to update the database");
+								});
 						}
 					});
 				}
@@ -63,17 +62,27 @@ centralEditorRoutes.route('/save').post(function (req, res) {
 centralEditorRoutes.route('/delete').delete(function (req, res) {
 	CentralEditor.find(function(err, editor){
 		if (err) {
-			console.log(err);
+			res.status(400).json(err);
 		} else {
 		    CentralEditor.findByIdAndRemove({_id: editor[0]._id}, function(err, editor){
 		        if (err) {
-		        	res.json(err);
+		        	res.status(400).json(err);
 		        } else {
-		        	res.json('Successfully removed');
+		        	res.status(200).json('Successfully removed');
 		        }
 		    });
 		}
 	});
+});
+
+centralEditorRoutes.route('/deleteById/:id').delete(function (req, res) {
+    CentralEditor.findByIdAndRemove({_id: req.params.id}, function(err, editor){
+        if (err) {
+        	res.status(400).json(err);
+        } else {
+        	res.status(200).json('Successfully removed');
+        }
+    });
 });
 
 module.exports = centralEditorRoutes;
