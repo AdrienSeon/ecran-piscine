480f00110cd5586e874d635d30da553cd4fad56d
Adrien SÉON <contact@adrienseon.com>
2019-07-04 15:30:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const config = require('./utils/dbConfig.js');
-
-let getIndex = require('./routes/index');
-let getObixPointDataRouter = require('./routes/get-obix-point-data');
-let CentralEditorRouter = require('./routes/CentralEditor.route');
-let BassinPointRouter = require('./routes/BassinPoint.route');
+const config = require('./utils/config.js');
+const getIndex = require('./routes/index');
+const getObixPointDataRouter = require('./routes/get-obix-point-data');
+const CentralEditorRouter = require('./routes/CentralEditor.route');
+const BassinPointRouter = require('./routes/BassinPoint.route');
 
 let app = express();
 
+const connectionString = 'mongodb://'+config.db.host+':'+config.db.port+'/'+config.db.name
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+mongoose.connect(connectionString, { useNewUrlParser: true }).then(
 	() => {console.log('Database is connected') },
 	err => { console.log('Can not connect to the database'+ err)}
 );
 
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
+//app.use(logger('dev'));
 
 app.use('/', getIndex);
 app.use('/get-obix-point-data', getObixPointDataRouter);
